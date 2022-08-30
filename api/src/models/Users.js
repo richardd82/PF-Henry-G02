584e01dc8801aa16101f1ba330da8c9eb3ffc27f
richardd82 <richardd82@gmail.com>
2022-08-30 13:12:08 -0500

    GitIgnore working

--- api/src/models/Users.js
+++ api/src/models/Users.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
+  // defino el modelo Users
   sequelize.define('users', {
     idUsers:{
       type: DataTypes.UUID,
