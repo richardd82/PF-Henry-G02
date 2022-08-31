2db109c8b407b9ebbfef00b89478948e4269e232
belÃmant <you@example.com>
2022-08-31 16:37:18 -0300

    users

--- api/src/Controllers/UsersControllers.js
+++ api/src/Controllers/UsersControllers.js
@@ -16,11 +16,11 @@ const createUser = async(req,res,next) =>{
     try {
         const {name, lastname, email, image, username, password, active, category}= req.body;
         const getAll= await Users.findAll();
-        console.log(getAll.users[0])
-        if (!getAll.users[0]){
+        // console.log(getAll.users[0])
+        // if (!getAll.users[0]){
         const newUser = await Users.create({name, lastname, email, image, username, password, active, category})
-        return res.json(newUser)}
-        else{return res.json({message:"Usuario ya existente"})}
+        return res.json(newUser)
+        // else{return res.json({message:"Usuario ya existente"})}
     }catch (error){
         console.log(error)
     }
