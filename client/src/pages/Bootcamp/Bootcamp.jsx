f2e49d328e7337f8dff21b72f1f10a30d30a1127
santiellis <Santiagoellis@hotmail.com.ar>
2022-08-31 19:31:25 -0300

    Models routing and Buttons, Assistance and Checkpoint Box, Bootcamp building

--- client/src/pages/Bootcamp/Bootcamp.jsx
+++ client/src/pages/Bootcamp/Bootcamp.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import AssistanceBox from "../../components/BootcampComponents/assistanceBox";
+import ButtonModule from "../../components/BootcampComponents/buttonModule";
+import CheckpointModal from "../../components/BootcampComponents/checkpointModal";
+import "./Bootcamp.css"
+
+const moduleArray = [
+  { module: 1 },
+  { module: 2 },
+  { module: 3 },
+  { module: 4 },
+  { module: 5 },
+];
 
 const Bootcamp = () => {
   return (
     <div>
-      <h1>Bootcamp</h1>
+      <div className="container">
+      <AssistanceBox />
+      <CheckpointModal/>
+      </div>
+      <ButtonModule title="Bootcamp" data={moduleArray.slice(0, 3)} />
+      <ButtonModule title="Labs" data={moduleArray.slice(3, 5)} />
     </div>
-  )
-}
+  );
+};
 
 export default Bootcamp;
