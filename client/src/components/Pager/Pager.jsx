80af1f7392019fa79bff594a878175083b28bc74
José Madriz <92412460+madrizjosea@users.noreply.github.com>
2022-08-31 21:43:38 -0300

    Create basic catalog with mocked data

--- client/src/components/Pager/Pager.jsx
+++ client/src/components/Pager/Pager.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
 
-const Pager = () => {
+const Pager = ({ currentPage, itemsPerPage, totalItems, dispatchHandler }) => {
+  // const dispatch = useDispatch();
+  // const pageNumbers = [];
+  const mockedPageNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  // Pages creation
+  // for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
+  //   pageNumbers.push(i);
+  // }
+
+  // Pagination handlers
+  const handlePrevious = e => {
+    e.preventDefault();
+    // if (currentPage - 1 > 0) {
+    //   dispatch(dispatchHandler(currentPage - 1));
+    // }
+  };
+
+  const handlePagination = (e, number) => {
+    e.preventDefault();
+    // dispatch(dispatchHandler(number));
+  };
+
+  const handleNext = e => {
+    e.preventDefault();
+    // if (currentPage + 1 <= mockedPageNumbers.length) {
+    //   dispatch(dispatchHandler(currentPage + 1));
+    // }
+  };
   return (
     <div>
-      <h1>Pager</h1>
+      <div>
+        {currentPage !== 1 ? (
+          <button onClick={e => handlePrevious(e)}>{'<'}</button>
+        ) : null}
+        {mockedPageNumbers.map(number => (
+          <button key={number} onClick={e => handlePagination(e, number)}>
+            {number}
+          </button>
+        ))}
+        {currentPage < mockedPageNumbers.length ? (
+          <button onClick={e => handleNext(e)}>{'>'}</button>
+        ) : null}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Pager;
--- client/src/pages/Catalog/Catalog.jsx
+++ client/src/pages/Catalog/Catalog.jsx
@@ -1,11 +1,56 @@
 import React from 'react';
+import SearchBar from '../../components/SearchBar/SearchBar.jsx';
+import Pager from '../../components/Pager/Pager.jsx';
+import Card from '../../components/Card/Card.jsx';
+
+const mockedVideos = [
+  {
+    thumbnail: `https://setevalapinsap.com/wp-content/plugins/wp-ulike/assets/img/no-thumbnail.png`,
+    instructor: 'Martina',
+    cohort: 'FT-20a',
+    title: 'Lecture - React',
+  },
+  {
+    thumbnail: `https://setevalapinsap.com/wp-content/plugins/wp-ulike/assets/img/no-thumbnail.png`,
+    instructor: 'Martina',
+    cohort: 'FT-20a',
+    title: 'Code Review - React',
+  },
+  {
+    thumbnail: `https://setevalapinsap.com/wp-content/plugins/wp-ulike/assets/img/no-thumbnail.png`,
+    instructor: 'Martina',
+    cohort: 'FT-20a',
+    title: 'Lecture - React-Redux',
+  },
+  {
+    thumbnail: `https://setevalapinsap.com/wp-content/plugins/wp-ulike/assets/img/no-thumbnail.png`,
+    instructor: 'Martina',
+    cohort: 'FT-20a',
+    title: 'Code Review - React-Redux',
+  },
+];
 
 const Catalog = () => {
   return (
     <div>
       <h1>Catalog</h1>
+      <SearchBar />
+      <Pager />
+      <div>
+        {mockedVideos.map((video, idx) => {
+          return (
+            <Card
+              key={idx}
+              title={video.title}
+              instructor={video.instructor}
+              cohort={video.cohort}
+            />
+          );
+        })}
+      </div>
+      <Pager />
     </div>
-  )
-}
+  );
+};
 
 export default Catalog;
