5df1c9a41c63dbb881ed7268616bfaed246c8c81
santiellis <Santiagoellis@hotmail.com.ar>
2022-08-31 22:01:32 -0300

    Changes in Bootcamp, added Modals and  Basic CSS

--- client/src/components/BootcampComponents/assistanceBox.jsx
+++ client/src/components/BootcampComponents/assistanceBox.jsx
@@ -4,16 +4,21 @@ import "./box.css";
 const asistSUp = { assistance: 90 };
 const userCohort = {cohort: "FT27b"}
 
+function openModal() {
+    document.querySelector(".modal-container").style.display = "flex"
+}
+
 export default function AssistanceBox() {
   return (<div>
     Cohorte Web{userCohort.cohort}
   <div>
     <div className="box">
-      <div className="position">
+      <div>
         <p>Asistencia al SUP</p>
         <div>
           <p>{asistSUp.assistance}%</p>
         </div>
+        <button onClick={openModal}>ℹ️</button>
       </div>
     </div>
   </div>
