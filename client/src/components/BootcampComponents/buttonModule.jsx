55f53ff9b4fae9b2e6e5f6ff2c2332c6dde16e57
santiellis <Santiagoellis@hotmail.com.ar>
2022-09-01 19:01:42 -0300

    New NavBar, new routes and nested display of modules

--- client/src/components/BootcampComponents/buttonModule.jsx
+++ client/src/components/BootcampComponents/buttonModule.jsx
@@ -4,20 +4,15 @@ import "./buttonModule.css"
 
 const ButtonModule = ({data, title, programs}) => {
   return (<div>
-        <h1>{title}</h1>
     <div className="position" >
       {data.map((obj, index) => {
         return (
           <Link key={obj.module} to={`/${title.toLowerCase()}/module/${obj.module}`}>
             <h1>{programs[index].program}</h1>
-            <button
-              id={obj.module}
-              name={"Module " + obj.module}
-              type="button"
+            <h1
             >
               M{`${obj.module}`}
-            </button>
-            <h5>See lessons</h5>
+            </h1>
           </Link>
         );
       })}
