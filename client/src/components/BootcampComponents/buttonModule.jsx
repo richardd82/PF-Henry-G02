41d3606197bde6f98b6c592457186a7115da2efc
santiellis <Santiagoellis@hotmail.com.ar>
2022-08-31 22:01:32 -0300

    Changes in Bootcamp, added Modals and  Basic CSS

--- client/src/components/BootcampComponents/buttonModule.jsx
+++ client/src/components/BootcampComponents/buttonModule.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./buttonModule.css"
 
-
-const ButtonModule = ({data, title}) => {
-  return (
-    <div>
+const ButtonModule = ({data, title, programs}) => {
+  return (<div>
         <h1>{title}</h1>
+    <div className="position" >
       {data.map((obj, index) => {
         return (
           <Link key={obj.module} to={`/${title.toLowerCase()}/module/${obj.module}`}>
+            <h1>{programs[index].program}</h1>
             <button
-              className="button-profile"
               id={obj.module}
               name={"Module " + obj.module}
               type="button"
             >
               M{`${obj.module}`}
             </button>
+            <h5>See lessons</h5>
           </Link>
         );
       })}
     </div>
+  </div>
   );
 };
 
--- client/src/pages/Bootcamp/Bootcamp.jsx
+++ client/src/pages/Bootcamp/Bootcamp.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import AssistanceBox from "../../components/BootcampComponents/assistanceBox";
+import AssistanceModale from "../../components/BootcampComponents/assistanceModale";
 import ButtonModule from "../../components/BootcampComponents/buttonModule";
+import Checkpoint from "../../components/BootcampComponents/checkpoint";
 import CheckpointModal from "../../components/BootcampComponents/checkpointModal";
 import "./Bootcamp.css"
 
@@ -10,17 +12,24 @@ const moduleArray = [
   { module: 3 },
   { module: 4 },
   { module: 5 },
+  { module: 6 },
 ];
+
+const programs = [{program: "Foundations"}, {program: "Front-End"}, {program: "Back-End"}, {program: "Data base"},{program: "Individual Proyect"}, {program: "Job Preparation"} ]
 
 const Bootcamp = () => {
   return (
     <div>
+      <AssistanceModale/>
+      <CheckpointModal/>
       <div className="container">
       <AssistanceBox />
-      <CheckpointModal/>
+       <Checkpoint/> 
       </div>
-      <ButtonModule title="Bootcamp" data={moduleArray.slice(0, 3)} />
-      <ButtonModule title="Labs" data={moduleArray.slice(3, 5)} />
+      <div >
+      <ButtonModule  programs={programs.slice(0,4)} title="Bootcamp" data={moduleArray.slice(0, 4)} />
+      </div>
+      <ButtonModule programs={programs.slice(4,6)} title="Labs" data={moduleArray.slice(4, 6)} />
     </div>
   );
 };
