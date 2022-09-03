9c6ab5aa566e70e24b5a27cb240678bbd4a703ab
José Madriz <92412460+madrizjosea@users.noreply.github.com>
2022-09-03 18:43:42 -0300

    Add: Link to Details from Card

--- client/src/components/Videos/Videos.jsx
+++ client/src/components/Videos/Videos.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Card from '../Card/Card';
 
 const Videos = ({ videos }) => {
   return (
     <div>
       {videos &&
-        videos.map((video) => {
+        videos.map(video => {
           return (
-            <Card
-              key={video.id}
-              title={video.name}
-              instructor={video.instructor}
-              cohort={video.cohortId}
-            />
+            <Link to='/bootcamp/lecture/1'>
+              <Card
+                key={video.id}
+                title={video.name}
+                instructor={video.instructor}
+                cohort={video.cohortId}
+              />
+            </Link>
           );
         })}
     </div>
