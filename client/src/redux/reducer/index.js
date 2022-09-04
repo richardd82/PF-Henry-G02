a552a732022657c6e9a4628f669f1b379329a71f
santiellis <Santiagoellis@hotmail.com.ar>
2022-09-03 22:09:21 -0300

    Back end connected to Bootcamp and Module

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import catalog from './catalogReducer.js';
+import bootcamp from "./bootcampReducer.js"
 
 export default combineReducers({
-  catalog,
+  catalog, bootcamp,
 });
