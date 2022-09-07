875c9c7745a39e4be137748fac4c964c04d0ae26
José Madriz <92412460+madrizjosea@users.noreply.github.com>
2022-09-06 21:04:23 -0300

    Componente CreateClass controlado y corrección de sintáxsis en actions

--- client/src/components/Teacher/CreateClass.js
+++ client/src/components/Teacher/CreateClass.js
@@ -1,113 +1,202 @@
 import React, { useEffect, useState } from 'react';
-import {useSelector, useDispatch} from 'react-redux'
-import { createClass, getCohorts, getModules } from '../../redux/actions/teacherActions';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  createClass,
+  getCohorts,
+  getModules,
+} from '../../redux/actions/teacherActions';
+// Error handler
+import { createClassErrors } from '../../helpers/setCreateClassErrors';
 
-export default function CreateClass (){
-    const [input, setInput] = useState({
+export default function CreateClass() {
+  const dispatch = useDispatch();
+  const modules = useSelector(state => state.teacher.modules);
+  const cohorts = useSelector(state => state.teacher.cohorts);
+
+  useEffect(() => {
+    dispatch(getCohorts());
+    dispatch(getModules());
+  }, [dispatch]);
+
+  const [input, setInput] = useState({
+    name: '',
+    lectureLink: '',
+    lectureLink2: '',
+    codeReviewLink: '',
+    codeReviewLink2: '',
+    description: '',
+    moduleId: '',
+    cohortId: '',
+    errorMsg: '',
+  });
+
+  const [warnings, setWarnings] = useState({
+    name: '',
+    lecture: '',
+    moduleId: '',
+    cohortId: '',
+    errorMsg: '',
+    error: false,
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setInput(prev => {
+      return {
+        ...prev,
+        errorMsg: '',
+        [name]: value,
+      };
+    });
+    setWarnings(createClassErrors(input));
+  }
+
+  function handleSelect(e) {
+    const { name, value } = e.target;
+    setInput(prev => {
+      return {
+        ...prev,
+        errorMsg: '',
+        [name]: value,
+      };
+    });
+    setWarnings(createClassErrors(input));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (warnings.error) {
+      setWarnings(prev => {
+        return {
+          ...prev,
+          errorMsg: 'Llena todos los campos obligatorios',
+        };
+      });
+    } else {
+      dispatch(createClass(input));
+      setInput({
         name: '',
-        lecture: '',
-        lecture2: '',
-        codeReview: '',
-        codeReview2: '',
+        lectureLink: '',
+        lectureLink2: '',
+        codeReviewLink: '',
+        codeReviewLink2: '',
         description: '',
         moduleId: '',
-        cohortId: ''
-    })
-
-    const modules = useSelector(state=> state.teacher.modules)
-    const cohorts = useSelector(state=> state.teacher.cohorts)
-
-
-    function handleChange (e){
-        const {name, value} = e.target
-        setInput(prev => {
-            return {
-                ...prev, 
-                [name]: value
-            }
-        })
+        cohortId: '',
+        errorMsg: '',
+      });
     }
+  }
 
-    function handleSelectModule (e){
-        const {value} = e.target
-        setInput(prev => {
-            return {
-                ...prev,
-                moduleId: value
-            }
-        })
-    }
+  console.log('INPUT', input);
+  console.log('WARNING', warnings);
 
-    function handleSelectCohorts (e){
-        const {value} = e.target
-        setInput(prev => {
-            return {
-                ...prev,
-                cohortId: value
-            }
-        })
-    }
-
-    return(
+  return (
+    <div>
+      <h1>Subir Clase</h1>
+      {warnings.errorMsg ? <p>{warnings.errorMsg}</p> : null}
+      <form onSubmit={e => handleSubmit(e)}>
         <div>
-            <h1>Subir clase</h1>
-            <fom>
-                <div>
-                <label>Nombre: </label>
-                <input type='text' name={'name'} value={input.name} placeholder={'nombre...'} onChange={(e)=> handleChange(e)}/>
-                </div>
-
-                <div>
-                <label>Lecture parte 1</label>
-                <input type='text' name={'leacture'} value={input.lecture} placeholder={'lecture...'} onChange={(e)=> handleChange(e)}/>
-                </div>
-
-                <div>
-                <label>Lecture parte 2</label>
-                <input type='text' name={'lecture2'} value={input.lecture2} placeholder={'lecture 2...'} onChange={(e)=> handleChange(e)}/>
-                </div>
-
-                <div>
-                <label>Code Review parte 1</label>
-                <input type='text' name={'codeReview'} value={input.codeReview} placeholder={'code review...'} onChange={(e)=> handleChange(e)}/>
-                </div>
-
-                <div>
-                <label>Code Review parte 2</label>
-                <input type='text' name={'codeReview2'} value={input.codeReview2} placeholder={'code review...'} onChange={(e)=> handleChange(e)}/>
-                </div>
-
-                <div>
-                <label>Descripción</label>
-                <textarea type='text' name={'description'} value={input.description} placeholder={'descripcion...'} onChange={(e)=> handleChange(e)}></textarea>
-                </div>
-
-                <div>
-                    <label>Modulos</label>
-                    <select onChange={(e)=> handleSelectModule(e)}>
-                        {
-                            modules && modules.map(e => {
-                                return (
-                                    <option value={e.id}>{e.name}</option> 
-                                )
-                            })
-                        }
-                    </select>
-                </div>
-
-                <div>
-                    <label>Cohortes</label>
-                    <select onChange={(e)=> handleChange(e)}>
-                        {
-                            cohorts && cohorts.map(e => {
-                                return (
-                                    <option value={e.id}>{e.name}</option> 
-                                )
-                            })
-                        }
-                    </select>
-                </div>
-            </fom>
+          <label>Nombre</label>
+          <input
+            type="text"
+            name="name"
+            value={input.name}
+            placeholder={'Nombre...'}
+            onChange={e => handleChange(e)}
+          />
+          {warnings.name ? <p>{warnings.name}</p> : null}
         </div>
-    )
+
+        <div>
+          <label>Lecture Parte 1</label>
+          <input
+            type="text"
+            name="lectureLink"
+            value={input.lectureLink}
+            placeholder={'Lecture...'}
+            onChange={e => handleChange(e)}
+          />
+          {warnings.lecture ? <p>{warnings.lecture}</p> : null}
+        </div>
+
+        <div>
+          <label>Lecture Parte 2</label>
+          <input
+            type="text"
+            name="lectureLink2"
+            value={input.lectureLink2}
+            placeholder={'Lecture 2...'}
+            onChange={e => handleChange(e)}
+          />
+        </div>
+
+        <div>
+          <label>Code Review Parte 1</label>
+          <input
+            type="text"
+            name="codeReviewLink"
+            value={input.codeReviewLink}
+            placeholder={'Code Review...'}
+            onChange={e => handleChange(e)}
+          />
+        </div>
+
+        <div>
+          <label>Code Review Parte 2</label>
+          <input
+            type="text"
+            name="codeReviewLink2"
+            value={input.codeReviewLink2}
+            placeholder={'Code Review...'}
+            onChange={e => handleChange(e)}
+          />
+        </div>
+
+        <div>
+          <label>Descripción</label>
+          <textarea
+            type="text"
+            name="description"
+            value={input.description}
+            placeholder={'Descripcion...'}
+            onChange={e => handleChange(e)}
+          ></textarea>
+        </div>
+
+        <div>
+          <label>Módulos</label>
+          <select name="moduleId" onChange={e => handleSelect(e)}>
+            {modules &&
+              modules.map(e => {
+                return (
+                  <option key={e.id} value={e.id}>
+                    {e.name}
+                  </option>
+                );
+              })}
+          </select>
+          {warnings.moduleId ? <p>{warnings.moduleId}</p> : null}
+        </div>
+
+        <div>
+          <label>Cohortes</label>
+          <select name="cohortId" onChange={e => handleSelect(e)}>
+            {cohorts &&
+              cohorts.map(e => {
+                return (
+                  <option key={e.id} value={e.id}>
+                    {e.name}
+                  </option>
+                );
+              })}
+          </select>
+          {warnings.cohortId ? <p>{warnings.cohortId}</p> : null}
+        </div>
+        <div>
+          <input type="submit" value="CREATE" />
+        </div>
+      </form>
+    </div>
+  );
 }
--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -3,10 +3,12 @@ import catalog from './catalogReducer.js';
 import searchBar from './searchBarReducer.js';
 import details from './detailReducer.js';
 import bootcamp from './bootcampReducer.js';
+import teacher from './teacherReducer.js';
 
 export default combineReducers({
   catalog,
   bootcamp,
   details,
   searchBar,
+  teacher,
 });
