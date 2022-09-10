9fcf3ddae1f254d230d8f947f359dc105c0e2a7b
cdbarre <87833829+cdbarre@users.noreply.github.com>
2022-09-10 16:50:21 -0300

    prueba anti cagada

--- client/src/components/Admin/Admin.jsx
+++ client/src/components/Admin/Admin.jsx
@@ -4,6 +4,7 @@ import UserAdmin from '../../pages/AdminProfile/UserAdmin'
 import Nav from '../Nav/Nav'
 import { FormsCreate } from './Forms/FormsCreate/FormsCreate'
 
+
 const Admin = ({user}) => {
   return (
     <div>
