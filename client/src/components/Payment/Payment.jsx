23382e7279eb8a1275b661022798cf212d9f3c94
José Madriz <92412460+madrizjosea@users.noreply.github.com>
2022-09-12 03:57:33 -0300

    Configuracion de pagos iniciada

---
+++ client/src/components/Payment/Payment.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from '../Nav/Nav';
+
+const Payment = ({ user }) => {
+  
+  return (
+    <div>
+      <Nav user={user} />
+      <h1>Pagar tus cuotas</h1>
+      <div id="cardPaymentBrick_container"></div>
+    </div>
+  );
+};
+
+export default Payment;
