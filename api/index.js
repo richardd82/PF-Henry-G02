95e94fb71120e7c36d2239cc98b33ca39d491387
richardd82 <richardd82@gmail.com>
2022-09-20 05:31:29 -0500

    Changes to Deploy in API

--- api/index.js
+++ api/index.js
@@ -19,10 +19,10 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
+let port = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(port, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
