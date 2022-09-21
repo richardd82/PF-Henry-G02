01ee7c2ed5a6617e45abbd461db8091d5491a62d
richardd82 <richardd82@gmail.com>
2022-09-21 06:15:42 -0500

    DELET

--- api/src/db.js
+++ api/src/db.js
@@ -33,6 +33,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_DIALECT } = process.env;
 // 				{ logging: false, native: false }
 // 		  );
 
+
 const sequelize = new Sequelize(`${DB_DIALECT}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
