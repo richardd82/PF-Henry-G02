f383dc65a40a3c5e5f2d00f055db175c100a37bc
richardd82 <richardd82@gmail.com>
2022-09-21 09:36:54 -0500

    Urls callback2

--- api/src/app.js
+++ api/src/app.js
@@ -31,7 +31,7 @@ const {
     res.header('Access-Control-Allow-Origin', CLIENT_URL); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Origin', CLIENT_URL+'/auth/google'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Origin', CLIENT_URL+'/auth/google/callback'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    // res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
